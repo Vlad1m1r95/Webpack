79b889b5b6ce469d954deb588567740b946862a7
Vladimir <vlad1im1r0195@gmail.com>
2019-09-06 17:30:43 +0300

    commit

--- src/App.js
+++ src/App.js
@@ -1,33 +1,126 @@
 import React from 'react'
 import $ from 'jquery'
+import { render } from 'react-dom';
 
+
+let result = ''
 export default class App extends React.Component {
-  componentDidMount() {
-    $('<h1 />')
-      .text('Hello world from JQuery')
-      .css({
-        textAlign: 'center',
-        color: '#2020c1'
-      })
-      .appendTo($('header'))
+  constructor(props) {
+    super(props);
+    this.state = {
+      num1: '',
+      num2: '',
+      value: '*'
+    };
+
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleSelectChange = this.handleSelectChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
   }
 
+  handleInputChange(event) {
+    const target = event.target;
+    const value = target.type === 'checkbox' ? target.checked : target.value;
+    const name = target.name;
+
+
+    this.setState({
+      [name]: value
+    });
+  }
+  handleSelectChange(event) {
+    this.setState({value: event.target.value});
+  }
+  
+  handleSubmit(event) {
+    event.preventDefault();
+    switch(this.state.value){
+      case  '+' :
+      result = Number(this.state.num1)   + Number(this.state.num2)
+      render(<Result />, document.getElementById('app')) 
+      break
+      case '-' :
+      result = Number(this.state.num1)   - Number(this.state.num2)
+      render(<Result />, document.getElementById('app')) 
+      break
+      case '/' :
+      result = Number(this.state.num1)   / Number(this.state.num2)
+      render(<Result />, document.getElementById('app')) 
+      break
+      case '*' :
+      result = Number(this.state.num1)   * Number(this.state.num2)
+      render(<Result />, document.getElementById('app')) 
+      break
+    }
+    
+   
+   
+    
+  }
+ 
   render() {
     return (
-      <React.Fragment>
-        <header></header>
-
-        <hr />
-
-        <div className="box">
-          <h2 className="box-title">Box title</h2>
-
-          <p className="box-text">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Minima quibusdam repellat incidunt quas facilis obcaecati sunt doloribus perferendis voluptas quis!</p>
+     
+        <div className="container">
+          <div className="row">
+            
+            <div className="col-md-12">
+            <form onSubmit={this.handleSubmit}  className="block">
+          <h1>React Калькулятор</h1>
+            {/* <div className="container">
+              <div className="row"></div>
+            </div> */}
+             <div className="form-element col-md-6 offset-md-3">
+               <label>Число 1</label>
+               <input type="number" name="num1"  value={this.state.num1} required onChange={this.handleInputChange} />
+               <label>Выберите дейсвие</label>
+               <select value={this.state.value} onChange={this.handleSelectChange} >
+                 <option value="*">Умножить</option>
+                 <option value="/">Разделить</option>
+                 <option value="+">Сложить</option>
+                 <option value="-">Вычесть</option>
+               </select>
+               <label>Число 2</label>
+               <input type="number"  name="num2" value={this.state.num2} required onChange={this.handleInputChange}/>
+               <button  type="submit">Рассчитать</button>
+             </div>
+         
+            </form>
+          </div>
         </div>
-        <div className="flex-autoprefixer">flex</div>
-        {/* <img src="C:\Work\Github\Webpack\src\img\Acute-Dog-Diarrhea-47066074.jpg"></img> */}
-         {/* <p>this text created in React</p> */}
-      </React.Fragment>
+        </div>
+     
     )
-  }
+  } 
 }
+
+ class Result extends App  {
+   render(){
+     return(
+       
+      <div className="container h-100">
+      <div className="row align-items-center h-100">
+          <div className="col-12 mx-auto">
+              <div className="jumbotron text-center  block-result">
+                  <h1 className="display-4">Ваш результат:</h1>
+                  <h1 className="display-6">{result}</h1>
+                  <p className="lead">
+                      <a onClick={returnHome.bind(this)}  className="btn btn-primary btn-color btn-lg" role="button">Вернуться</a>
+                  </p>
+              </div>
+          </div>
+      </div>
+  </div>
+    
+     )
+   }
+  
+}
+const returnHome = () => {
+  event.preventDefault()
+  render(<App />, document.getElementById('app'))
+}
+
+
+
+
